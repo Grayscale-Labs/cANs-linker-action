3431b3e30ab0410b3ef3d61bd2d326f4bb650d1a
kphan32 <kavin.phan@grayscaleapp.com>
2022-08-09 10:10:57 -0400

    Setup project and create open action to link ticket with PR

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { debug, setFailed } from "@actions/core";
+import { context } from "@actions/github";
+import { PullRequestEvent } from "@octokit/webhooks-types";
+import opened from "./opened";
+
+async function run() {
+  debug("Running cANs-linker-action");
+
+  if (context.eventName !== "pull_request") {
+    setFailed("This action only works with `pull_request` events");
+    return;
+  }
+
+  const payload = context.payload as PullRequestEvent;
+
+  switch (payload.action) {
+    case "opened":
+      debug("Running `opened` task");
+
+      return opened();
+    default:
+      setFailed(
+        "This action only works with the `opened` action for `pull_request` events"
+      );
+
+      return;
+  }
+}
+
+run();
